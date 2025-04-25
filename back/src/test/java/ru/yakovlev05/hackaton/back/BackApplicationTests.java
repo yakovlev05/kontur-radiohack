20225a0eb63f60c664dcf3a1ba78c8cc70b7ebbd
yakovlev05 <alexey.yakovlev.05@mail.ru>
2025-04-25 10:42:00 +0500

    Создание проекта back

---
+++ back/src/test/java/ru/yakovlev05/hackaton/back/BackApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.yakovlev05.hackaton.back;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
