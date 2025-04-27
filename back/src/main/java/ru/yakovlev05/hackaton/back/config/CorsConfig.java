8dcb18795d12dbfc18aada5bd264c51ff692ad1b
yakovlev05 <alexey.yakovlev.05@mail.ru>
2025-04-27 12:15:48 +0500

    Настройка CORS

---
+++ back/src/main/java/ru/yakovlev05/hackaton/back/config/CorsConfig.java
@@ -0,0 +1,23 @@
+package ru.yakovlev05.hackaton.back.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+
+import java.util.List;
+
+@Configuration
+public class CorsConfig {
+
+    @Bean
+    UrlBasedCorsConfigurationSource corsConfigurationSource() {
+        CorsConfiguration configuration = new CorsConfiguration();
+        configuration.setAllowedOrigins(List.of("*"));
+        configuration.setAllowedMethods(List.of("*"));
+
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        source.registerCorsConfiguration("/**", configuration);
+        return source;
+    }
+}
