4a6a40a6097e2c846b2f0b7ccebbb2227ce246a1
Alexey Yakovlev <144012782+yakovlev05@users.noreply.github.com>
2025-05-05 20:01:33 +0500

    Реальный ip адрес клиента (за traefik) (#73)

--- back/src/main/java/ru/yakovlev05/hackaton/back/controller/GameController.java
+++ back/src/main/java/ru/yakovlev05/hackaton/back/controller/GameController.java
@@ -2,8 +2,6 @@ package ru.yakovlev05.hackaton.back.controller;
 
 import jakarta.servlet.http.HttpServletRequest;
 import lombok.RequiredArgsConstructor;
-import org.hibernate.mapping.Collection;
-import org.hibernate.mapping.List;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -22,14 +20,6 @@ public class GameController {
     @PostMapping
     public CreateGameResponseDto createGame(@RequestBody CreateGameRequestDto createGameRequestDto,
                                             HttpServletRequest request) {
-        var headers = request.getHeaderNames();
-        while (headers.hasMoreElements()){
-            var header = headers.nextElement();
-            System.out.printf("%s:%s%n", header, request.getHeader(header));
-        }
-        System.out.println("----------------------");
-        System.out.println("----------------------");
-        System.out.println("----------------------");
         return gameService.createGame(createGameRequestDto, request);
     }
 }
